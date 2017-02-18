97103c0e5599989f80e80c8f445c1f87e048826f
Karol <trzcinskikar@gmail.com>
2017-02-18 18:14:49 +0100

    added comments for post, new view with one post view by id. adding comments without login user

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Post;
+use AppBundle\Entity\Comment;
+use AppBundle\Form\CommentType;
 
 class DefaultController extends Controller
 {
@@ -26,6 +29,35 @@ class DefaultController extends Controller
         );
         return $this->render('default/index.html.twig', array(
             'posts' => $pagination
+        ));
+    }
+    /**
+     * @Route("/article/{id}", name="show_post")
+     */
+    public function showAction(Post $post, Request $request){
+        
+        $comment = new Comment();
+        $comment->setPost($post);
+        
+        //  $comment->setUser($user);
+        
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        
+        if($form->isValid() ){
+           $em = $this->getDoctrine()->getManager();
+           $em->persist($comment);
+           $em->flush();
+            
+            $this->addFlash('success', 'Komentarz został dodany');
+            
+            return $this->redirectToRoute('show_post', array('id' => $post->getId()));
+        }
+        
+        
+        return $this->render('default/show.html.twig', array(
+            'post' => $post ,
+            'form' => $form->createView()
         ));
     }
 }
---
+++ src/AppBundle/Form/CommentType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class CommentType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('content', TextareaType::class, array(
+            'label' => false,
+            'attr' => array('placeholder' => 'Treść komentarza')
+        ));
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Comment'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_comment';
+    }
+
+
+}
