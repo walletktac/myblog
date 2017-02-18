449950b29c873ad86ff763f0519d89d6f8d6c6b7
Karol <trzcinskikar@gmail.com>
2017-02-18 20:23:27 +0100

    added fos userBundle, added login-form. added user, user can only see and adding comments

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,55 +9,61 @@ use AppBundle\Entity\Post;
 use AppBundle\Entity\Comment;
 use AppBundle\Form\CommentType;
 
-class DefaultController extends Controller
-{
+
+class DefaultController extends Controller {
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
-        $qb= $this->getDoctrine()
+    public function indexAction(Request $request) {
+        $qb = $this->getDoctrine()
                 ->getManager()
                 ->createQueryBuilder()
                 ->from('AppBundle:Post', 'p')
                 ->select('p');
         $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
-                $qb,
-                $request->query->get('page', 1),
-                20
+                $qb, $request->query->get('page', 1), 20
         );
         return $this->render('default/index.html.twig', array(
-            'posts' => $pagination
+                    'posts' => $pagination
         ));
     }
+
     /**
      * @Route("/article/{id}", name="show_post")
      */
-    public function showAction(Post $post, Request $request){
-        
-        $comment = new Comment();
-        $comment->setPost($post);
-        
-        //  $comment->setUser($user);
-        
-        $form = $this->createForm(CommentType::class, $comment);
-        $form->handleRequest($request);
-        
-        if($form->isValid() ){
-           $em = $this->getDoctrine()->getManager();
-           $em->persist($comment);
-           $em->flush();
+    public function showAction(Post $post, Request $request) {
+
+        $form = null;
+
+        //jeśli użytkownik jest zalogowany
+        if ($user = $this->getUser()) {
             
-            $this->addFlash('success', 'Komentarz został dodany');
-            
-            return $this->redirectToRoute('show_post', array('id' => $post->getId()));
+            $comment = new Comment();
+            $comment->setPost($post);
+            $comment->setUser($user);
+
+
+            $form = $this->createForm(CommentType::class, $comment);
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($comment);
+                $em->flush();
+
+                $this->addFlash('success', 'Komentarz został dodany');
+
+                return $this->redirectToRoute('show_post', array('id' => $post->getId()));
+            }
         }
-        
-        
+
         return $this->render('default/show.html.twig', array(
-            'post' => $post ,
-            'form' => $form->createView()
+                    'post' => $post,
+                    'form' => is_null($form) ? $form : $form->createView()
         ));
     }
+
 }
